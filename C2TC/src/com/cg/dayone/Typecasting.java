e1e1bc804a855977def27b769d90f468ddfce7a0
SUGANYA P <SUGANYA P@LAPTOP-4OERPREG>
2023-07-30 12:09:03 +0530

    dayone

---
+++ C2TC/src/com/cg/dayone/Typecasting.java
@@ -0,0 +1,15 @@
+package com.cg.dayone;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 23;
+	    System.out.println("The integer value: " + n);
+
+	    // convert into double type
+	    float change = n;
+	    System.out.println("The float value: " + change);
+	}
+
+}
