cbf740334fe5b66a6e946e87c48d0db2cb04d7b3
SUGANYA P <SUGANYA P@LAPTOP-4OERPREG>
2023-07-30 12:02:06 +0530

    dayone

---
+++ C2TC/src/com/cg/dayone/identify.java
@@ -0,0 +1,11 @@
+package com.cg.dayone;
+
+public class identify {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Full Stack Web Development");
+
+	}
+
+}
