93a985626f1b349a5681cfd538fddf101bf25ee6
SUGANYA P <SUGANYA P@LAPTOP-4OERPREG>
2023-07-30 11:53:18 +0530

    Dayone

---
+++ C2TC/src/com/cg/dayone/Datatypes.java
@@ -0,0 +1,30 @@
+package com.cg.dayone;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = true;
+	    System.out.println(a); 
+	    byte b=10;
+	    System.out.println(b);
+	    short celcius=-100;
+	    System.out.println(celcius);
+	    int c=20;
+	    System.out.println(c);
+	    long d = 6233230L;
+	    System.out.println(d); 
+	    double e = -328.3;
+	    System.out.println(e);
+	    float f = -56.3f;
+	    System.out.println(f); 
+	    char letter = '\u0052';
+	    System.out.println(letter); 
+	    char l1 = '8';
+	    System.out.println(l1); 	
+	    char l2 = 67;
+	    System.out.println(l2);
+	    
+	}
+
+}
