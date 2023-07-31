ffc718958273e4b07b87bd0ec2a6f5e66602d28c
SUGANYA P <SUGANYA P@LAPTOP-4OERPREG>
2023-07-31 09:49:51 +0530

    daytwo

---
+++ C2TC/src/com/cg/daytwo/basics.java
@@ -0,0 +1,18 @@
+package com.cg.daytwo;
+
+public class basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=23;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a++);
+		System.out.println(a%b);
+		System.out.println(a/b);
+		System.out.println(--b);
+		int sum=0;
+	}
+
+}
