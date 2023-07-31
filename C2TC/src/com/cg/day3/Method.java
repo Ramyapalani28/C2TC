b253d44496fed026281aaec5209405103e0dbd35
SUGANYA P <SUGANYA P@LAPTOP-4OERPREG>
2023-07-31 14:37:50 +0530

    first

---
+++ C2TC/src/com/cg/day3/Method.java
@@ -0,0 +1,14 @@
+package com.cg.day3;
+
+public class Method {
+
+	public static void main(String[] args) {
+		 Encap e=new Encap();
+		 e.setAcccount_num(6474783);
+		 e.setID(01);
+		 e.setName("ramya");
+		 System.out.println(e.toString());
+		 
+	}
+
+}
